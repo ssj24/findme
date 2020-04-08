68aadb013b4569cdf934307e75c3d312669083cb
sujee <totquf824@gmail.com>
2020-04-08 23:07:22 +0900

    S02P22B204-22 Update account page

---
+++ front/src/views/user/AccountJob.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <p>추천 공고</p>
+    <v-container fluid>
+      <v-layout wrap>
+        <v-flex 
+          v-for="card in cards"
+          :key="card.title"
+          class="ma-0 pa-0"
+          xs12 sm5 md4 lg3
+          >
+          <v-card class="job-card">
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.3), rgba(0,0,0,.8)"
+              height="200px"
+            >
+              <v-card-title class="font-weight-bold" v-text="card.company"></v-card-title>
+              <v-card-subtitle class="white--text font-weight-black text-right mb-n6">{{ card.position }}</v-card-subtitle>
+              <span class="d-flex justify-end">
+                <v-card-subtitle 
+                  v-for="stack in card.stacks"
+                  :key="stack"
+                  class="white--text font-weight-medium d-inline-block mb-n6 pl-1"
+                  >
+                  {{ stack }}
+                </v-card-subtitle>
+              </span>
+              <v-card-subtitle class="white--text font-weight-medium text-right">~{{ card.date }}</v-card-subtitle>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    
+  </div>  
+</template>
+
+<script>
+export default {
+  data: () => ({
+      cards: [
+        { company: '퀸즈코퍼레이션', position: '개발팀장', stacks: ['보안', '웹개발'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { company: '트래포트', position: '프론트', stacks: ['java', 'oracle'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+        { company: '윈즈시스템', position: '보안장비 기술 엔지니어', stacks: ['보안'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { company: '클라우드네트웍스', position: '개발자', stacks: ['java', 'javascript', 'python'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { company: '베스핀글로벌', position: 'DSA', stacks: ['java', 'python'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+        { company: '시프트원', position: '프론트', stacks: ['react'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+      ],
+    }),
+}
+</script>
+
+<style scoped>
+.theme--light.v-card {
+  box-shadow: 3px 3px 5px #ccc;
+}
+</style>
