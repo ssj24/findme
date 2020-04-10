16ed707d3e8c7ee0ca6264391798ee0c58e4836f
sujee <totquf824@gmail.com>
2020-04-10 13:46:13 +0900

    S02P22B204-66 Update swiper

--- front/src/views/user/AccountJob.vue
+++ front/src/views/user/AccountJob.vue
@@ -57,12 +57,12 @@
 export default {
   data: () => ({
       cards: [
-        { company: '퀸즈코퍼레이션', position: '개발팀장', stacks: ['보안', '웹개발'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
-        { company: '트래포트', position: '프론트', stacks: ['java', 'oracle'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
-        { company: '윈즈시스템', position: '보안장비 기술 엔지니어', stacks: ['보안'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-        { company: '클라우드네트웍스', position: '개발자', stacks: ['java', 'javascript', 'python'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
-        { company: '베스핀글로벌', position: 'DSA', stacks: ['java', 'python'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
-        { company: '시프트원', position: '프론트', stacks: ['react'], date: '2020-04-17', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { company: '퀸즈코퍼레이션', position: '개발팀장', stacks: ['보안', '웹개발'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', flex: 3 },
+        { company: '트래포트', position: '프론트', stacks: ['java', 'oracle'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60', flex: 3 },
+        { company: '윈즈시스템', position: '보안장비 기술 엔지니어', stacks: ['보안'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60', flex: 3 },
+        { company: '클라우드네트웍스', position: '개발자', stacks: ['java', 'javascript', 'python'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60', flex: 3 },
+        { company: '베스핀글로벌', position: 'DSA', stacks: ['java', 'python'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60', flex: 3 },
+        { company: '시프트원', position: '프론트', stacks: ['react'], date: '2020-04-17', src: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60', flex: 3 },
       ],
     }),
 }
