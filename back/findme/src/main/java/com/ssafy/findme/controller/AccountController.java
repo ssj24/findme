b2cc91f3f9c6e6f8b9dd57a04666223207921ca7
JangEunJeong <dmswjd6481@naver.com>
2020-04-19 22:29:45 +0900

    S02P23B204-30, S02P23B204-29 | Update signup+smtp, login+jwt and Add scheduler

--- back/findme/src/main/java/com/ssafy/findme/controller/AccountController.java
+++ back/findme/src/main/java/com/ssafy/findme/controller/AccountController.java
@@ -1,19 +1,20 @@
 package com.ssafy.findme.controller;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.findme.domain.Users;
+import com.ssafy.findme.domain.User;
 import com.ssafy.findme.service.IAccountService;
-import com.ssafy.findme.service.UserMailSendService;
 import com.ssafy.findme.service.UserSha256;
 
 import io.swagger.annotations.ApiOperation;
@@ -25,64 +26,39 @@ public class AccountController {
 
 	@Autowired
 	private IAccountService accountservice;
-	@Autowired
-	private UserMailSendService mailsender;
 
-//	@GetMapping("/users")
-//	@ApiOperation(value = "전체사용자조회")
-//	@ResponseBody
-//	public List<Users> list() {
-//		try {
-//			List<Users> memberList = accountservice.findAll();
-//			return memberList;
-//		} catch (DataIntegrityViolationException e) {
-//			System.out.println("history already exist");
-//		}
-//		return null;
-//	}
-//
-//	@PostMapping("/insert")
-//	@ApiOperation(value = "사용자추가")
-//	public void insert(@RequestBody Users user) {
-//		try {
-////			Users users = Users.builder().user_email(user.getUser_email()).user_pwd(user.getUser_pwd())
-////					.user_name(user.getUser_name()).build();
-//			accountservice.save(user);
-//		} catch (DataIntegrityViolationException e) {
-//			System.out.println("insert fail");
-//		}
-//	}
+	@GetMapping("/user/{email}/email-duplicate")
+	@ApiOperation(value = "이메일 중복체크")
+	public boolean emailduplicate(@PathVariable String email) {
+		return accountservice.emailDuplicateCheck(email);
+	}
 
 	@PostMapping("/user/signup")
 	@ApiOperation(value = "가입하기")
-	public void signup(@RequestBody Users user) throws MessagingException {
+	public void signup(@RequestBody User user) throws MessagingException {
 		// 비밀번호 암호화
-		String encryPassword = UserSha256.encrypt(user.getUser_pwd());
-		user.setUser_pwd(encryPassword);
+		String encryPassword = UserSha256.encrypt(user.getPassword());
+		user.setPassword(encryPassword);
 		// 회원가입
 		accountservice.signUp(user);
 		// 인증메일
 		accountservice.mailSendWithUserKey(user.getEmail(), user.getName());
-
 	}
 
-	//	@PostMapping("/insert")
-//	@ApiOperation(value = "사용자추가")
-//	public void insert(@RequestBody Users user) {
-//	    try {
-//	    	accountservice.save(users);
-//	    } catch (DataIntegrityViolationException e) {
-//	        System.out.println("insert fail");
-//	    }
-//	}
-	
-	
-	
 	@GetMapping("/user/key_alter")
 	@ApiOperation(value = "이메일 인증하기")
-	public void authentication(@RequestParam("user_email") String email, @RequestParam("user_key") String key) {
-
+	public void authentication(@RequestParam("email") String email, @RequestParam("key") String key) {
 		accountservice.alterUserKey(email, key);
+	}
 
+	@PostMapping("/user/login")
+	@ApiOperation(value = "로그인")
+	public String login(@RequestParam String email, @RequestParam String password, HttpServletResponse res) {
+		User user = new User();
+		user.setEmail(email);
+		user.setPassword(password);
+		String token = accountservice.login(user);
+		res.setHeader("jwt-auth-token", token);
+		return token;
 	}
 }
