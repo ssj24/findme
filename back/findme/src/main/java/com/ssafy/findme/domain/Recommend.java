a8a07d01d5beeb5b752b73e55eca226372eaba5a
김가은 <l9960204@naver.com>
2020-04-21 15:59:28 +0900

    2P23B204-55, 2P23B204-56, 2P23B204-58|Update Similarity Pearson

---
+++ back/findme/src/main/java/com/ssafy/findme/domain/Recommend.java
@@ -0,0 +1,34 @@
+package com.ssafy.findme.domain;
+
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Table(name = "recommend")
+@Getter
+@Setter
+
+public class Recommend {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "recruit_id")
+	private Recruit recruit;
+}
