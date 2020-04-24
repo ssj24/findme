07f3ac1a48f7e970056ceaaaea7b29c9f5068fb3
김가은 <l9960204@naver.com>
2020-04-24 17:05:27 +0900

    S02P23B204-55, S02P23B204-56, S02P23B204-58, S02P23B204-59|Update Survey, TextMining

--- back/findme/src/main/java/com/ssafy/findme/repository/SurveyRepository.java
+++ back/findme/src/main/java/com/ssafy/findme/repository/SurveyRepository.java
@@ -3,9 +3,11 @@ package com.ssafy.findme.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.findme.domain.Survey;
 
+@Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long>{
 	List<Survey> findByLanguageId(Long language_id);
 	Survey findByUserIdAndLanguageId(Long user_id, Long language_id);
---
+++ back/findme/src/main/java/com/ssafy/findme/service/ITextMiningService.java
@@ -0,0 +1,7 @@
+package com.ssafy.findme.service;
+
+import com.ssafy.findme.dto.TextMiningDTO;
+
+public interface ITextMiningService {
+	TextMiningDTO findById(Long id);
+}
