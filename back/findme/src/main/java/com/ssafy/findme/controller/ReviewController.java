d399e819c134011105a42c229333fcffa35357db
김가은 <l9960204@naver.com>
2020-04-25 16:19:14 +0900

    S02P23B204|Update Review, Symp, Unsymp

--- back/findme/src/main/java/com/ssafy/findme/controller/ReviewController.java
+++ back/findme/src/main/java/com/ssafy/findme/controller/ReviewController.java
@@ -35,7 +35,7 @@ public class ReviewController {
 		reviewservice.save(user_id, content, language_id);
 	}
 
-	@PutMapping("/review/{review_id}/update")
+	@PutMapping("/review/update/{review_id}")
 	@ApiOperation(value = "리뷰 업데이트")
 	@ResponseBody
 	public void update(@PathVariable Long review_id, @RequestParam String content) {
@@ -45,22 +45,24 @@ public class ReviewController {
 	@GetMapping("/review/findAll/{language_id}")
 	@ApiOperation(value = "프로그래밍 언어별 전체 리뷰 조회")
 	@ResponseBody
-	public List<ReviewDTO> findAllByLanguageId(@PathVariable Long id) {
-		return reviewservice.findAllByLanauageId(id);
+	public List<ReviewDTO> findAllByLanguageId(@PathVariable Long language_id) {
+		return reviewservice.findAllByLanauageId(language_id);
 	}
 
-	@DeleteMapping("/review/{review_id}/delete")
+	@DeleteMapping("/review/delete/{review_id}")
 	@ApiOperation(value = "리뷰 삭제")
 	public void delete(@PathVariable Long review_id) {
 		reviewservice.delete(review_id);
 	}
 
-	@GetMapping("/review/findAll/{language_id}")
+	@GetMapping("/review/findAll/{language_id}/user/{user_id}")
 	@ApiOperation(value = "사용자별 작성 리뷰 리스트")
-	public List<ReviewDTO> findReviewByUserIdAndLanguageId(@PathVariable Long language_id, @RequestParam Long user_id) {
+	public List<ReviewDTO> findReviewByUserIdAndLanguageId(@PathVariable Long language_id, @PathVariable Long user_id) {
 		return reviewservice.findReviewByUserIdAndLanguageId(user_id, language_id);
 	}
 	
+	
+	//리뷰 공감, 비공감
 	@GetMapping("/review/symp/findAll/{language_id}")
 	@ApiOperation(value = "리뷰 공감 리스트")
 	@ResponseBody
@@ -68,13 +70,13 @@ public class ReviewController {
 		return reviewservice.findAllSymp();
 	}
 	
-	@PostMapping("/review/symp/{review_id}/save")
+	@PostMapping("/review/symp/save/{review_id}")
 	@ApiOperation(value = "하나의 리뷰 공감 누름")
-	public void saveSymp(@PathVariable Long review_id, @RequestParam Long user_id) {
-		reviewservice.saveSymp(review_id, user_id);
+	public boolean saveSymp(@PathVariable Long review_id, @RequestParam Long user_id) {
+		return reviewservice.saveSymp(review_id, user_id);
 	}
 	
-	@GetMapping("/review/symp/{user_id}/findAll")
+	@GetMapping("/review/symp/findAll/{user_id}")
 	@ApiOperation(value = "사용자가 공감 누른 리뷰 리스트")
 	@ResponseBody
 	public List<SympDTO> findSympByUserId(@PathVariable Long user_id) {
@@ -94,13 +96,13 @@ public class ReviewController {
 		return reviewservice.findAllUnsymp();
 	}
 	
-	@PostMapping("/review/unsymp/{review_id}/save")
+	@PostMapping("/review/unsymp/save/{review_id}")
 	@ApiOperation(value = "하나의 리뷰 비공감 누름")
-	public void saveUnsymp(@PathVariable Long review_id, @RequestParam Long user_id) {
-		reviewservice.saveUnsymp(review_id, user_id);
+	public boolean saveUnsymp(@PathVariable Long review_id, @RequestParam Long user_id) {
+		return reviewservice.saveUnsymp(review_id, user_id);
 	}
 	
-	@GetMapping("/review/unsymp/{user_id}/findAll")
+	@GetMapping("/review/unsymp/findAll/{user_id}")
 	@ApiOperation(value = "사용자가 비공감 누른 리뷰 리스트")
 	@ResponseBody
 	public List<UnsympDTO> findUnsympByUserId(@PathVariable Long user_id) {
--- back/findme/src/main/java/com/ssafy/findme/repository/UnsympRepository.java
+++ back/findme/src/main/java/com/ssafy/findme/repository/UnsympRepository.java
@@ -9,6 +9,6 @@ import com.ssafy.findme.domain.Unsymp;
 
 @Repository
 public interface UnsympRepository extends JpaRepository<Unsymp, Long> {
-	List<Unsymp> findByUser_Id(Long user_id);
-	Unsymp findByReview_IdAndUser_Id(Long review_id, Long user_id);
+	List<Unsymp> findByUserId(Long user_id);
+	Unsymp findByReviewIdAndUserId(Long review_id, Long user_id);
 }
