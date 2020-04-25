79a07da12d10c8eddb94cb51335484b2fb90233d
JangEunJeong <dmswjd6481@naver.com>
2020-04-25 16:49:46 +0900

    S02P23B204-49 | Add kakao additional agreement

--- back/findme/src/main/java/com/ssafy/findme/service/AccountServiceImpl.java
+++ back/findme/src/main/java/com/ssafy/findme/service/AccountServiceImpl.java
@@ -1,14 +1,6 @@
 package com.ssafy.findme.service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
---
+++ back/findme/src/main/python/kakaoSendToMe.py
@@ -0,0 +1,78 @@
+import pymysql
+import pandas as pd
+import requests
+import json
+import sys
+
+def main(access_token, recruit_id):
+    recruit_id=str(recruit_id)
+    conn = pymysql.connect(host='localhost', user='root', password='ssafy', db='test', charset='utf8')
+    curs = conn.cursor()
+    # sql = "select * from recruits where recruit_id = (select recruit_id from recommend where user_id = "+user_id+" ) "
+    sql = "select * from recruits where recruit_id = " + recruit_id
+
+    curs.execute(sql)
+    rows = curs.fetchall()
+    rows_data = pd.DataFrame(rows)
+    print(rows_data)
+    print(rows_data[2])
+    print(rows_data[4])
+    # access_token="DJtWKRhhc9mQaqltzi5ytmH7hCMryOKBFZj0AgopcNIAAAFxrO3kqA"
+    like_cnt=100
+    title = rows_data[2][0]
+    description = rows_data[4][0]
+    image_url="http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg"
+
+    template_dict_data = str({
+        "object_type": "feed",
+        "content": {
+            "title": title,
+            "description": description,
+            "image_url": image_url,
+            "image_width": 640,
+            "image_height": 640,
+            "link": {
+                "web_url": "http://www.daum.net",
+                "mobile_web_url": "http://m.daum.net",
+                "android_execution_params": "contentId=100",
+                "ios_execution_params": "contentId=100"
+            }
+        },
+        "social": {
+            "like_count": like_cnt
+        },
+        "buttons": [
+            {
+                "title": "웹으로 이동",
+                "link": {
+                    "web_url": "http://www.daum.net",
+                    "mobile_web_url": "http://m.daum.net"
+                }
+            },
+            {
+                "title": "앱으로 이동",
+                "link": {
+                    "android_execution_params": "contentId=100",
+                    "ios_execution_params": "contentId=100"
+                }
+            }
+        ]
+    })
+
+    kakao_to_me_uri = 'https://kapi.kakao.com/v2/api/talk/memo/default/send'
+
+
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Authorization': "Bearer " + access_token,
+    }
+
+    template_json_data = "template_object="+str(json.dumps(template_dict_data))
+    template_json_data = template_json_data.replace("\"", "")
+    template_json_data = template_json_data.replace("'", "\"")
+    
+    response = requests.request(method="POST", url=kakao_to_me_uri, data=template_json_data, headers=headers)
+    print(response.json())
+
+if __name__ == "__main__":
+    main(sys.argv[1] , sys.argv[2])
