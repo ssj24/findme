44012c62c45c42cbc758824c98117a5b6de20a65
JangEunJeong <dmswjd6481@naver.com>
2020-04-29 19:27:02 +0900

    S02P23B204-76 | Update recruit data type

--- back/findme/src/main/java/com/ssafy/findme/repository/SaraminRepository.java
+++ back/findme/src/main/java/com/ssafy/findme/repository/SaraminRepository.java
@@ -75,41 +75,41 @@ public interface SaraminRepository extends JpaRepository<SaraminData, Long> {
 	List<SaraminData> findByJavasTrue();
 
 	List<SaraminData> findByAssemblysTrue();
-
+//
 	List<SaraminData> findByCppsTrue();
-
+//
 	List<SaraminData> findByCsTrue();
-
+//
 	List<SaraminData> findByCsharpsTrue();
-
+//
 	List<SaraminData> findByDelphisTrue();
-
+//
 	List<SaraminData> findByGosTrue();
-
+//
 	List<SaraminData> findByJssTrue();
-
+//
 	List<SaraminData> findByMatlabsTrue();
-
+//
 	List<SaraminData> findByOcsTrue();
-
+//
 	List<SaraminData> findByPhpsTrue();
-
+//
 	List<SaraminData> findByPerlsTrue();
-
+//
 	List<SaraminData> findByPlsTrue();
-
+//
 	List<SaraminData> findByPythonsTrue();
-
+//
 	List<SaraminData> findByRsTrue();
-
+//
 	List<SaraminData> findByRubysTrue();
-
+//
 	List<SaraminData> findBySqlsTrue();
-
+//
 	List<SaraminData> findBySwiftsTrue();
-
+//
 	List<SaraminData> findByVbnsTrue();
-
-	List<SaraminData> findByVbs();
+//
+	List<SaraminData> findByVbsTrue();
 
 }
