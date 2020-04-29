fcbfd4a34a95ffffa5aa29ecd8f5fdc198dee272
JangEunJeong <dmswjd6481@naver.com>
2020-04-30 04:42:36 +0900

    Update kakao signin/signup and mail

--- front/src/App.vue
+++ front/src/App.vue
@@ -65,7 +65,7 @@ export default {
       menuItems: [
         { title: "Statistics", path: "/statistics", icon: "mdi-chart-bar", forIf: true },
         { title: "User", path: "/account", icon: "mdi-face", forIf: true },
-        { title: "SignIn", path: "/signmerge", icon: "mdd-open-variant-outline", forIf: false },
+        { title: "SignIn", path: "/signmerge", icon: "mdi-lock-open-outline", forIf: false },
         { title: "SignOut", path: "/signmerge", icon: "mdi-lock-outline", forIf: true },
       
       ]
